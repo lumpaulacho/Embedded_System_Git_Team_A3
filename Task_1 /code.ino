9efe803f5b84051311da79973a95cba9aaef282f
lumpaulacho <paula-cho.lum@stud.hshl.de>
2025-10-06 01:44:39 +0200

    Create code.ino

---
+++ Task_1 /code.ino
@@ -0,0 +1,25 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(7,HIGH);
+  delay(5000);
+  digitalWrite(7,LOW);
+
+  digitalWrite(6,HIGH);
+  delay(1000);
+  digitalWrite(6,LOW);
+
+  digitalWrite(5,HIGH);
+  delay(5000);
+  digitalWrite(5,LOW);
+
+  digitalWrite(6,HIGH);
+  delay(1000);
+  digitalWrite(6,LOW);
+}
